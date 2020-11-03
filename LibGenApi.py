41d0e98e5eebd426e666acca9aed94978fb67b01
zrezke <jeretina.filip@gmail.com>
2020-11-03 14:25:12 +0100

    Basic functionality works. Implemented scraper for searching, started implementing scraping result sites with link argument given (branch: issue-1)

--- LibGenApi.py
+++ LibGenApi.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
-import os
+import sys
+
 
 app = Flask(__name__)
 
@@ -12,48 +13,104 @@ api = Api(app)
 class Scraper(object):
     def __init__(self, args):
         self.args = args
-        self.bs4 = BeautifulSoup()
+        url = ""
 
     def search(self):
-        if self.args['search_in'] == "LibGen (Sci-Tech)":
-            query = self.args['query']
-            if len(query) < 2:
-                return {"error":"404 Query must be at least 2 characters."}, 404
+        url = self.args['link']
+        url = url.replace(" ", "+")
+        url = url.replace('"AND"', "&")
+        url = url.replace("[]", "")
+        url = "http://libgen.li/" + url
+        if url == None:
+            if self.args['search_in'] == "LibGen (Sci-Tech)":
+                query = self.args['query']
+                query = query.replace(" ", "+")
+                if len(query) < 2:
+                    return {"error":"404 Query must be at least 2 characters."}, 404
 
-            mask = self.args['search_with_mask'] #  0 == Yes; 1 == No
-            column = self.args['search_in_fields']
-            if column is None:
-                column = "def"
-            if mask is None:
-                mask = "1"
+                mask = self.args['search_with_mask'] #  0 == Yes; 1 == No
+                column = self.args['search_in_fields']
+                if column is None:
+                    column = "def"
+                if mask is None:
+                    mask = "1"
             
-            url = f"http://libgen.rs/search.php?req={query}&lg_topic=libgen&open=0&view=simple&res=100&phrase={mask}&column={column}"
-            self.source = requests.get(url)
-            return query
+                url = f"http://libgen.rs/search.php?req={query}&lg_topic=libgen&open=0&view=simple&res=100&phrase={mask}&column={column}"
 
-    #  class of search reult table == "c"
-    #  than find tbody
-    #  than every td in tbody except first one
-    #  than find elements under td i think its mostly <a>
-    def scrape(self):
-        pass
+        self.source = requests.get(url)
+        return self.searchScrape()
 
 
+    #  searchScrape()
+    '''
+        Returns this:
+        {
+            "bookId": {
+                "author": ["author", "linkToAuthorSearch"],
+                "title": "Title",
+                "publisher": "Publisher",
+                "year": "xxxx",
+                "pages": "xxx",
+                "language": "Language",
+                "size": "500Kb",
+                "extension": "rar",
+                "mirrors": ["linktoMiror", "linkToMirror" ...]
+            },
+        }
+    '''
+    def searchScrape(self):
+        soup = BeautifulSoup(self.source.text, 'html.parser')
+        table = soup.find_all("tr", recursive=True)
+        result = {}
+        for tr in table:
+            td = tr.find_all("td")
+            if len(td) <= 15 and len(td) >= 12:
+                return str(td)
+                bookId = td[0].text
+                result[bookId] = {}
+                thisBook = result[bookId]
+                author = td[1].find("a")
+                link = author["href"]
+                author = author.text
+                thisBook["author"] = (author, link)
+                title = td[2].find(id=bookId)
+                thisBook["title"] = title.contents[0]
+                thisBook["publisher"] = td[3].text
+                thisBook["year"] = td[4].text
+                thisBook["pages"] = td[5].text
+                thisBook["language"] = td[6].text
+                thisBook["size"] = td[7].text
+                thisBook["extension"] = td[8].text
+                mirrors = []
+                for element in td[9:14]:
+                    try:
+                        a = element.find("a")
+                        mirrors.append(a["href"])
+                    except TypeError:
+                        pass
+                thisBook["mirrors"] = mirrors
+
+        return result, 200
+
+
+        
 class SearchLibGen(Resource, Scraper):
     def __init__(self):
         super().__init__(Scraper)
 
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('query', required=True)
-        parser.add_argument('search_in', required=True)
-        parser.add_argument('search_with_mask', required=False)
-        parser.add_argument('search_in_fields', required=False)
+        parser.add_argument('type', required=True)
         self.args = parser.parse_args()
-        self.search()
-        #  Make method for passing args into crawler
-
-        return {"message": "nig"}, 200
+        if self.args['type'] == "search":
+            parser.add_argument('query', required=True)
+            parser.add_argument('search_in', required=True)
+            parser.add_argument('search_with_mask', required=False)
+            parser.add_argument('search_in_fields', required=False)
+            #  When Adding links use "AND" instead of &!!!!!
+            parser.add_argument('link', required=False)
+            self.args = parser.parse_args()
+            return self.search()
 
     def post(self):
         return ("404 This API doesn't have post support.", 404)
@@ -64,13 +121,6 @@ class SearchLibGen(Resource, Scraper):
 
 
 api.add_resource(SearchLibGen, '/searchLibGen')
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
